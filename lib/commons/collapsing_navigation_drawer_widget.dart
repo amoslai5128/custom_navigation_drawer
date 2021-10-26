7361960627b40c1e880f66d33d9e6b92ecacd18d
AmosLai <alexlai5128@gmail.com>
2021-10-26 15:44:31 +0800

    Update collapsing_navigation_drawer_widget.dart

--- lib/commons/collapsing_navigation_drawer_widget.dart
+++ lib/commons/collapsing_navigation_drawer_widget.dart
@@ -57,11 +57,11 @@ class CollapsingNavigationDrawerState extends State<CollapsingNavigationDrawer> 
 
     _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
     if (isCollapsed) {
-      widthAnimation = Tween<double>(begin: minWidth, end: maxWidth).animate(_animationController);
-      _animationController.reverse();
-    } else {
       widthAnimation = Tween<double>(begin: maxWidth, end: minWidth).animate(_animationController);
       _animationController.forward();
+    } else {
+      widthAnimation = Tween<double>(begin: minWidth, end: maxWidth).animate(_animationController);
+      _animationController.reverse();
     }
   }
 
