94876f2f68e8fa62b8b486ace6fc9ea3e1f5ebb8
AmosLai <alexlai5128@gmail.com>
2021-11-24 15:36:35 +0800

    Update example code

--- example/main.dart
+++ example/main.dart
@@ -34,11 +34,60 @@ class MyHomePage extends StatelessWidget {
             Container(
               color: selectedColor,
             ),
-            CollapsingNavigationDrawer(
-              navigationModels: [],
-              userName: '',
-            ),
+            BaseNavigationDrawer(),
           ],
         ));
   }
+}
+
+class BaseNavigationDrawer extends StatelessWidget {
+  const BaseNavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: CollapsingNavigationDrawer(
+        isCollapsedByDefault: true,
+        elevation: 0,
+        backgroundColor: KColors.primary,
+        openedDrawIconColor: KColors.passive,
+        closedDrawIconColor: KColors.background,
+        userIconColor: KColors.background,
+        itemIconColor: KColors.passive,
+        userNameTextStyle: Theme.of(context).textTheme.headline4?.copyWith(color: KColors.background),
+        itemTextStyle: Theme.of(context).textTheme.bodyText1?.copyWith(color: KColors.background),
+        selectedIconColor: Colors.white,
+        selectedItemBackgroundColor: KColors.primaryLight.withOpacity(0.3),
+        selectedItemTextStyle: Theme.of(context).textTheme.bodyText2?.copyWith(color: Colors.white),
+        userName: 'Amos',
+        userIconWidget: Image.asset("assets/images/user_1.png", width: 30),
+        buttonCopyrightWidget: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Image.asset("assets/images/app_logo_white.png", width: 30),
+        ),
+        navigationModels: [
+          NavigationModel(
+            title: 'Comparasion',
+            icon: Icons.home,
+            onTap: () => {} /* Navigator.popAndPushNamed(context,  ) */,
+          ),
+          NavigationModel(title: 'Second Page', icon: Icons.settings, onTap: () {})
+        ],
+      ),
+    );
+  }
+}
+
+class KColors {
+  static const background = Colors.white;
+
+  /// Card, Textfield, widgets backgound
+  static const base = Color(0xFFF4F5F9);
+
+  static const primary = Color(0xFF4F5DE0);
+  static const primaryLight = Color(0xFFE2E7FB);
+
+  // Colors for status, buttons
+  /// Textfield, Boarder
+  static const passive = Color(0xFFD0D1E1);
 }
